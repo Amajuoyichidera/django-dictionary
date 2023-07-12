110a7efc0a88027429e60ef8d646b6ba79b37054
Amajuoyi chidera <amajuoyichideradavid@gmail.com>
2023-07-12 23:05:56 +0100

    first version

--- dictionary/views.py
+++ dictionary/views.py
@@ -14,8 +14,18 @@ def word(request):
     synonyms = dictionary.synonym(search)
     antonyms = dictionary.antonym(search)
     context = {
-        'meaning' : meaning['Noun'][0],
+        'meaning' : meaning,
         'synonyms' : synonyms,
         'antonyms' : antonyms,
     }
     return render(request, 'word.html', context)
+
+
+# def word(request):
+#     search = request.GET.get('search')
+#     dictionary = PyDictionary()
+#     meaning = dictionary.meaning(search)
+#     context = {
+#         'meaning' : meaning
+#     }
+#     return render (request, 'word.html', context)
